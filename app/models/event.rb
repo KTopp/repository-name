dbc13e4dcd237656e5a68d3c0c07145b55db5075
Gaston de la Fare <gastondelafare@gmail.com>
2024-12-05 15:33:33 -0300

    search bar for events + minor corrections

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,11 +1,11 @@
 class Event < ApplicationRecord
-  has_many :tickets
-
   include PgSearch::Model
   pg_search_scope :global_search,
                   against: %i[name location],
                   using: {
                     tsearch: { prefix: true }
                   }
+
   # validations...
+  has_many :tickets
 end
